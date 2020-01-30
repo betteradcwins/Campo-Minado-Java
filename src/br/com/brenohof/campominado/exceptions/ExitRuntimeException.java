4429fbcefa1c7ce3539327b64d336e878cc49823
Breno H. Oliveira <brenohenriqueof@gmail.com>
2020-01-30 15:31:12 -0300

    Deleted exceptions, Application.java and BoardConsole.java.

--- src/br/com/brenohof/campominado/exceptions/ExitRuntimeException.java
+++
@@ -1,5 +0,0 @@
-package br.com.brenohof.campominado.exceptions;
-
-public class ExitRuntimeException extends RuntimeException {
-    private static final long serialVersionUID = 1;
-}
--- src/br/com/brenohof/campominado/exceptions/ExplosionRuntimeException.java
+++
@@ -1,5 +0,0 @@
-package br.com.brenohof.campominado.exceptions;
-
-public class ExplosionRuntimeException extends RuntimeException {
-    private static final long serialVersionUID = 1;
-}
